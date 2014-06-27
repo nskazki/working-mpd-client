93297a433cd60e2d1bc9d9517a1491ca9081792d
nskazki <nskazki@gmail.com>
2014-06-27 04:20:36 +0400

    fix bug in Command.toString

--- mpd.js
+++ mpd.js
@@ -281,7 +281,7 @@ function Command(rawCommand) {
 Command.prototype.toString = function() {
 	return this.cmd + " " + this.args.map(function(arg) {
 		// replace all " with \"
-		return '"' + arg.toString().replace(/"/g, '\\"') + '"';
+		return arg ? ('"' + arg.toString().replace(/"/g, '\\"') + '"') : ' ';
 	}).join(" ") + "\n";
 };
 
