4375be6e35eb1e73a92d22b65c66d525f36cf5bd
nskazki <nskazki@gmail.com>
2019-10-20 07:54:00 +0300

    Add example project

---
+++ example/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const MpdClient = require('../')
+const mpdClient = new MpdClient({
+  connectOptions: {
+    host: 'localhost',
+    port: 6600
+  },
+  reconnectOptions: {
+    isUse: true,
+    reconnectDelay: 2000
+  }
+}).on('error', console.error)
+  .on('changed', (name) => {
+    if (name == "playlist") printPlaylist()
+  })
+  .on('ready', printPlaylist)
+  .init()
+
+function printPlaylist() {
+  mpdClient.sendCommand('playlist', function(err, result) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('\nnew playlist:\n' + result)
+    }
+  })
+}
